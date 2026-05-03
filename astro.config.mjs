be4a50f16053d8d65031e621ac4af2202b60c494
19cashj <67474732+19cashj@users.noreply.github.com>
2026-05-03 09:13:01 -0400

    Keystatic Init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import markdoc from '@astrojs/markdoc';
+import keystatic from '@keystatic/astro';
 
 export default defineConfig({
   site: 'https://19cashj.github.io',
-  base: '/ccs-mascots/',
+  base: '/',
+  integrations: [react(), markdoc(), keystatic()],
 });
