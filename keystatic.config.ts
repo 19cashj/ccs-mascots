524670b6deee491807c411503b6da5065df9a573
19cashj <67474732+19cashj@users.noreply.github.com>
2026-05-06 15:42:15 -0400

    Converted img to Image components. Small fixes and changes to keystatic

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -16,8 +16,8 @@ export default config({
         price: fields.text({ label: 'Price' }),
         image: fields.image({
           label: 'Image',
-          directory: 'public/images',
-          publicPath: '/images/',
+          directory: 'src/images',
+          publicPath: '/src/images/',
         }),
         description: fields.text({ label: 'Description' }),
       },
@@ -29,67 +29,79 @@ export default config({
       path: 'src/content/home',
       format: { data: 'json' },
       schema: {
-        heroHeadlineLine1: fields.text({ label: 'Hero Headline Line 1', defaultValue: 'Costumes and' }),
-        heroHeadlineLine2: fields.text({ label: 'Hero Headline Line 2', defaultValue: 'Fursuits, Maine' }),
-        heroHeadlineLine3: fields.text({ label: 'Hero Headline Line 3', defaultValue: 'Made Your Way' }),
-        heroTagline: fields.text({
-          label: 'Hero Tagline',
-          defaultValue: 'Affordable, Durable, Adorable Custom Costumes made just for you!',
-        }),
-        heroPrimaryButtonText: fields.text({ label: 'Hero Primary Button Text', defaultValue: 'Get a Quote' }),
-        heroPrimaryButtonUrl: fields.url({
-          label: 'Hero Primary Button URL',
-          defaultValue:
-            'https://docs.google.com/forms/d/e/1FAIpQLSdvfWdapp_bzKXglitzdxh65CiGRf7PifEItHlmHBU5zKwX_Q/viewform',
-        }),
-        heroSecondaryButtonText: fields.text({ label: 'Hero Secondary Button Text', defaultValue: 'Terms of Service' }),
-        heroSecondaryButtonUrl: fields.url({
-          label: 'Hero Secondary Button URL',
-          defaultValue: 'https://docs.google.com/document/d/1TyKMxOpOX514vxL4cl_tLCc27aMnGeRZZWeNfWybef0/edit?tab=t.0',
-        }),
-        pricingTitle: fields.text({ label: 'Pricing Section Title', defaultValue: 'Price Guide' }),
-        pricingDisclaimer: fields.text({
-          label: 'Pricing Disclaimer',
-          defaultValue:
-            'Prices listed are for basic with no added extras. Costs may increase to reflect added details and design difficulty.',
-        }),
-        aboutTitle: fields.text({ label: 'About Section Title', defaultValue: 'About Us' }),
-        aboutBody: fields.text({
-          label: 'About Section Body',
-          defaultValue:
-            'Local to the Maine Furry Community since 2014, we have prided ourselves on providing Quality, Affordable Mascot Style Costumes. We use innovative methods such as 3D modeling and printing, as well as the finest fabrics and materials, to create a comfortable and durable costume that will last for years to come.',
-        }),
-        aboutImage: fields.image({
-          label: 'About Image',
-          directory: 'public/images',
-          publicPath: '/images/',
-        }),
-        ctaTitle: fields.text({
-          label: 'CTA Title',
-          defaultValue: 'Ready to Bring Your Character to Life?',
-        }),
-        ctaSubtitle: fields.text({
-          label: 'CTA Subtitle',
-          defaultValue: 'Custom fursuits built with love, right here in Maine.',
-        }),
-        ctaImage: fields.image({
-          label: 'CTA Background Image',
-          directory: 'public/images',
-          publicPath: '/images/',
-        }),
-        ctaButtonText: fields.text({ label: 'CTA Button Text', defaultValue: 'Get a Quote' }),
-        ctaButtonUrl: fields.url({
-          label: 'CTA Button URL',
-          defaultValue:
-            'https://docs.google.com/forms/d/e/1FAIpQLSdvfWdapp_bzKXglitzdxh65CiGRf7PifEItHlmHBU5zKwX_Q/viewform',
-        }),
-        footerTagline: fields.text({
-          label: 'Footer Tagline',
-          defaultValue:
-            'Delivering smiles to the Furry Community and Costume Enthusiasts alike since 2014',
-        }),
-        footerLocation: fields.text({ label: 'Footer Location', defaultValue: 'Augusta ME, 04330' }),
-        footerEmail: fields.text({ label: 'Footer Email', defaultValue: 'CraftyWolf@ccsmascots.net' }),
+        hero: fields.object({
+          heroHeadlineLine1: fields.text({ label: 'Headline Line 1', defaultValue: 'Costumes and' }),
+          heroHeadlineLine2: fields.text({ label: 'Headline Line 2', defaultValue: 'Fursuits, Maine' }),
+          heroHeadlineLine3: fields.text({ label: 'Headline Line 3', defaultValue: 'Made Your Way' }),
+          heroTagline: fields.text({
+            label: 'Tagline',
+            defaultValue: 'Affordable, Durable, Adorable Custom Costumes made just for you!',
+          }),
+          heroPrimaryButtonText: fields.text({ label: 'Primary Button Text', defaultValue: 'Get a Quote' }),
+          heroPrimaryButtonUrl: fields.url({
+            label: 'Primary Button URL',
+            defaultValue:
+              'https://docs.google.com/forms/d/e/1FAIpQLSdvfWdapp_bzKXglitzdxh65CiGRf7PifEItHlmHBU5zKwX_Q/viewform',
+          }),
+          heroSecondaryButtonText: fields.text({ label: 'Secondary Button Text', defaultValue: 'Terms of Service' }),
+          heroSecondaryButtonUrl: fields.url({
+            label: 'Secondary Button URL',
+            defaultValue: 'https://docs.google.com/document/d/1TyKMxOpOX514vxL4cl_tLCc27aMnGeRZZWeNfWybef0/edit?tab=t.0',
+          }),
+        }, { label: 'Hero' }),
+        pricingGuide: fields.object({
+          pricingTitle: fields.text({ label: 'Title', defaultValue: 'Price Guide' }),
+          pricingDisclaimer: fields.text({
+            label: 'Disclaimer',
+            defaultValue:
+              'Prices listed are for basic with no added extras. Costs may increase to reflect added details and design difficulty.',
+          }),
+        }, { label: 'Pricing Guide' }),
+        about: fields.object({
+          aboutTitle: fields.text({ label: 'Title', defaultValue: 'About Us' }),
+          aboutBody: fields.text({
+            label: 'Body',
+            defaultValue:
+              'Local to the Maine Furry Community since 2014, we have prided ourselves on providing Quality, Affordable Mascot Style Costumes. We use innovative methods such as 3D modeling and printing, as well as the finest fabrics and materials, to create a comfortable and durable costume that will last for years to come.',
+          }),
+          aboutImage: fields.image({
+            label: 'Image',
+            directory: 'src/images',
+            publicPath: '/src/images/',
+          }),
+        }, { label: 'About' }),
+        cta: fields.object({
+          ctaTitle: fields.text({
+            label: 'Title',
+            defaultValue: 'Ready to Bring Your Character to Life?',
+          }),
+          ctaSubtitle: fields.text({
+            label: 'Subtitle',
+            defaultValue: 'Custom fursuits built with love, right here in Maine.',
+          }),
+          ctaImage: fields.image({
+            label: 'Background Image',
+            directory: 'src/images',
+            publicPath: '/src/images/',
+          }),
+          ctaButtonText: fields.text({ label: 'Button Text', defaultValue: 'Get a Quote' }),
+          ctaButtonUrl: fields.url({
+            label: 'Button URL',
+            defaultValue:
+              'https://docs.google.com/forms/d/e/1FAIpQLSdvfWdapp_bzKXglitzdxh65CiGRf7PifEItHlmHBU5zKwX_Q/viewform',
+          }),
+        }, { label: 'CTA' }),
+        footer: fields.object({
+          footerTagline: fields.text({
+            label: 'Tagline',
+            defaultValue: 'Delivering smiles to the Furry Community and Costume Enthusiasts alike since 2014',
+          }),
+          footerLocation: fields.text({ label: 'Location', defaultValue: 'Augusta ME, 04330' }),
+          footerEmail: fields.text({ label: 'Email', defaultValue: 'CraftyWolf@ccsmascots.net' }),
+          facebookUrl: fields.url({ label: 'Facebook URL', defaultValue: 'https://www.facebook.com/CCSMascots/' }),
+          instagramUrl: fields.url({ label: 'Instagram URL', defaultValue: 'https://www.instagram.com/ccsmascots/' }),
+          tiktokUrl: fields.url({ label: 'TikTok URL', defaultValue: 'https://www.tiktok.com/@ccsmascots' }),
+        }, { label: 'Footer' }),
       },
     }),
     galleries: singleton({
@@ -97,33 +109,19 @@ export default config({
       path: 'src/content/galleries',
       format: { data: 'json' },
       schema: {
-        marqueeGalleryImages: fields.array(
+        galleryImages: fields.array(
           fields.object(
             {
               image: fields.image({
                 label: 'Image',
-                directory: 'public/images',
-                publicPath: '/images/',
-              }),
-              alt: fields.text({ label: 'Alt Text', defaultValue: '' }),
-            },
-            { label: 'Marquee Image' }
-          ),
-          { label: 'Marquee Gallery Images', defaultValue: [] }
-        ),
-        galleryPageImages: fields.array(
-          fields.object(
-            {
-              image: fields.image({
-                label: 'Image',
-                directory: 'public/images',
-                publicPath: '/images/',
+                directory: 'src/images',
+                publicPath: '/src/images/',
               }),
               alt: fields.text({ label: 'Alt Text', defaultValue: '' }),
             },
             { label: 'Gallery Image' }
           ),
-          { label: 'Gallery Page Images', defaultValue: [] }
+          { label: 'Gallery Images' }
         ),
       },
     }),
