8c7bf5ea650a634e450976c9cd31c4270c46bc3c
19cashj <67474732+19cashj@users.noreply.github.com>
2026-05-07 13:10:30 -0400

    Randomized Marquee, Hero height fix, keystatic cloud config

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -3,7 +3,10 @@ import { config, fields, singleton, collection } from '@keystatic/core';
 
 export default config({
   storage: {
-    kind: 'local',
+    kind: 'cloud',
+  },
+  cloud: {
+    project: 'ccs-mascots/ccs-mascots',
   },
   collections: {
     pricingGuideCards: collection({
